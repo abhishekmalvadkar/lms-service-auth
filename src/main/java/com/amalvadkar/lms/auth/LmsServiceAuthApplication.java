bd5fc1487a874e8378bdb679a94150b3edbcec26
Abhishek Malvadkar <abhi@dev.com>
2025-02-15 10:10:48 +0530

    initial commit

---
+++ src/main/java/com/amalvadkar/lms/auth/LmsServiceAuthApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServiceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsServiceAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/lms/auth/LmsServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lms.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsServiceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
