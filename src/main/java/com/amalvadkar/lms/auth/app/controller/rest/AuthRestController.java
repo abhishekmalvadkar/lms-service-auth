50896b7268ce94cd0da57b2f465b4df0938b226a
Abhishek Malvadkar <abhi@dev.com>
2025-02-17 23:00:02 +0530

    GH-1 : Implemented the verify account API

--- src/main/java/com/amalvadkar/lms/auth/app/controller/rest/AuthRestController.java
+++ src/main/java/com/amalvadkar/lms/auth/app/controller/rest/AuthRestController.java
@@ -1,6 +1,7 @@
 package com.amalvadkar.lms.auth.app.controller.rest;
 
 import com.amalvadkar.lms.auth.app.models.request.CreateAccountReq;
+import com.amalvadkar.lms.auth.app.models.request.VerifyAccountRequest;
 import com.amalvadkar.lms.auth.app.models.resonse.CustomResModel;
 import com.amalvadkar.lms.auth.app.services.AuthService;
 import jakarta.validation.Valid;
@@ -16,12 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthRestController {
 
+    public static final String ENDPOINT_VERIFY_ACCOUNT = "/verify-account";
+    public static final String ENDPOINT_CREATE_ACCOUNT = "/create-account";
+
     private final AuthService authService;
 
 
-    @PostMapping("/create-account")
+    @PostMapping(ENDPOINT_CREATE_ACCOUNT)
     public ResponseEntity<CustomResModel> CreateAccount(@Valid @RequestBody CreateAccountReq createAccountReq) {
         return ResponseEntity.ok(authService.createAcoount(createAccountReq));
+    }
+
+    @PostMapping(ENDPOINT_VERIFY_ACCOUNT)
+    public ResponseEntity<CustomResModel> verifyAccount(@RequestBody VerifyAccountRequest verifyAccountRequest){
+      return ResponseEntity.ok(authService.verifyAccount(verifyAccountRequest));
+
     }
 
 
