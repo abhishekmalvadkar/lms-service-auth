42e2f29950ede2db2b15e52f25c0b30452d9d846
Abhishek Malvadkar <abhi@dev.com>
2025-03-06 15:36:49 +0530

    GH-1 : Added saviour log exception method in global exception handler class

--- src/main/java/com/amalvadkar/lms/auth/app/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/amalvadkar/lms/auth/app/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.amalvadkar.lms.auth.app.exception.handler;
 
 import com.amalvadkar.lms.auth.app.exception.ResourceAlreadyExistsException;
 import com.amalvadkar.lms.auth.app.models.resonse.CustomResModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<CustomResModel> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex){
+        logException(ex);
         return ResponseEntity.status(HttpStatus.valueOf(ex.getCode()))
                 .body(CustomResModel.fail(List.of(ex.getMessage()), ex.getCode()));
+    }
+
+    private static void logException(Throwable ex) {
+        log.error("Exception occurred : ", ex);
     }
 
 }
