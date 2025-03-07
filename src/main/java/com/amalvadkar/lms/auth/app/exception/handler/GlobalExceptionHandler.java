ca7c56f9839592a930c12def86bfcec0bb43d24b
Abhishek Malvadkar <abhi@dev.com>
2025-03-07 20:24:14 +0530

    GH-1 : Refactored GlobalExceptionHandler

--- src/main/java/com/amalvadkar/lms/auth/app/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/amalvadkar/lms/auth/app/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.amalvadkar.lms.auth.app.exception.handler;
 
-import com.amalvadkar.lms.auth.app.exception.ResourceAlreadyExistsException;
+import com.amalvadkar.lms.auth.app.exception.AuthException;
 import com.amalvadkar.lms.auth.app.models.resonse.CustomResModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,17 +10,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     private static final String EXCEPTION_OCCURRED_MSG = "Exception occurred : ";
+    private static final String SOMETHING_WENT_WRONG_ERR_MSG = "Something went wrong, please try later";
 
-    @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<CustomResModel> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex){
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<CustomResModel> handleAuthException(AuthException ex){
         logException(ex);
         return ResponseEntity.status(HttpStatus.valueOf(ex.getCode()))
                 .body(CustomResModel.fail(List.of(ex.getMessage()), ex.getCode()));
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<CustomResModel> handleThrowable(Throwable ex){
+        logException(ex);
+        return ResponseEntity.internalServerError()
+                .body(CustomResModel.fail(List.of(SOMETHING_WENT_WRONG_ERR_MSG) ,
+                        INTERNAL_SERVER_ERROR.value()));
     }
 
     private static void logException(Throwable ex) {
